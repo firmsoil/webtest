b73e7fa06bb9d6f9b50dabfb40cd6af361bbd567
firmsoil <firmsoil@gmail.com>
2016-01-24 20:21:02 -0800

    Update configCDPipeline.groovy

--- configCDPipeline.groovy
+++ configCDPipeline.groovy
@@ -1,5 +1,9 @@
 /**
- * Loads properties from groovy file
+ * Loads properties from file
  */
 
-def configCDPipeline = new ConfigSlurper().parse(new File('https://github.com/firmsoil/webtest/blob/master/configCDPipelineProps.groovy').text)
+// def configCDPipeline = new ConfigSlurper().parse(new File('https://github.com/firmsoil/webtest/blob/master/configCDPipelineProps.groovy').text)
+
+def propFile = loadPropertiesFile('configCDPipelineProps.properties')
+   //Integer port = propFile.getInt('server.port')
+   String url = propFile.get('artifactoryURL')
